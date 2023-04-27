292d339ca696fb30a283c7659f5d7c824dfb3a69
ugurcanaltun <ucaltun@gmail.com>
2023-04-27 22:44:40 +0300

    zort

--- mps.c
+++ mps.c
@@ -708,7 +708,7 @@ int main(int argc, char* argv[])
             pthread_mutex_destroy(&loadNumMutex[i]);
         }
         free(queueMutex);
-        free(loadNumMutex)
+        free(loadNumMutex);
     }
     
     if(strcmp(qs,"LM") == 0){
